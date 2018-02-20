713db9bb0a87a16dc337082a4a0885d2ca11b23f
themihai <mihai@epek.com>
2018-02-21 01:41:39 +0200

    fix error handling

--- error.go
+++ error.go
@@ -1,12 +1,36 @@
 package tvdb
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
 // JSONError is a generic type for errors
 type JSONError struct {
 	Message string `json:"error,omitempty"`
+}
+
+func (j *JSONError) UnmarshalJSON(p []byte) error {
+	type MessageErr struct {
+		Message string `json:"error,omitempty"`
+	}
+	var v MessageErr
+	if err := json.Unmarshal(p, &v); err == nil {
+		j.Message = v.Message
+		return nil
+	}
+	/*
+	   "errors": {
+	   		 "invalidLanguage": "Incomplete or no translation for the given language"
+	 	 }
+	*/
+	type ErrorMap map[string]string
+	var v2 ErrorMap
+	err := json.Unmarshal(p, &v2)
+	if err == nil {
+		j.Message = fmt.Sprintf("%q", err)
+	}
+	return err
 }
 
 func (e JSONError) Error() string {
